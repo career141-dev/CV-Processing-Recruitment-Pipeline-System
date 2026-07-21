022c7ea002b4841879875b7b7d1f037704559ef3
HDBinath <bytecreator3@gmail.com>
2026-07-21 11:01:27 +0530

    feat: add R2 storage integration and configure GitHub Actions deployment pipeline

--- convex/storage/r2.ts
+++ convex/storage/r2.ts
@@ -1,3 +1,5 @@
+"use node";
+
 import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { action, internalAction } from "../_generated/server";
